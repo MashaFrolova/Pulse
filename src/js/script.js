5d4045bab9facddeecfeffa2f4508de60997f76c
Masha <manjunyafrolova@gmail.com>
2021-03-23 13:22:09 +0300

    index_refresh

--- src/js/script.js
+++ src/js/script.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         adaptiveHeight: true,
-        centerMode: true,
-        variableWidth: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
